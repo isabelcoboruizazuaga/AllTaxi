ea8654c1e50eb4a2511e86bb9f6d8088fe309c0f
Isa <isabelcoboruizazuaga@gmail.com>
2024-05-14 09:56:56 +0200

    vinculated player animations

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,7 +11,29 @@ public class PlayerController : MonoBehaviour
     public float velocidadInicial;
     public float sprint = 1;
 
+    public Animator anim;
 
+    public bool IsWalking
+    {
+        get { return _walking; }
+        set
+        {
+            if (_walking != value)
+            {
+                _walking = value;
+                if (_walking == true)
+                {
+                    anim.SetBool("walking", true);
+                }
+                else
+                {
+                    anim.SetBool("walking", false);
+                }
+            }
+        }
+    }
+
+    private bool _walking=false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,19 +47,28 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
         //Lectura cursores
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
+        if (y != 0 && y != 0)
+        {
+            IsWalking = true;
+        }
+        else
+        {
+            IsWalking = false;
+        }
+
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             sprint = 1.5f;
+            anim.SetFloat("sprint", sprint);
         }
         if (Input.GetKeyUp(KeyCode.LeftShift))
         {
             sprint = 1f;
+            anim.SetFloat("sprint", sprint);
         }
     }
 
@@ -51,10 +82,10 @@ public class PlayerController : MonoBehaviour
     //Para las fisicas
     private void FixedUpdate()
     {
-
         //Movimiento
         transform.Rotate(0, x * Time.fixedDeltaTime * velocidadRotacion, 0);
         transform.Translate(0, 0, y * Time.fixedDeltaTime * velocidadMovimiento * sprint);
+
     }
 
 
