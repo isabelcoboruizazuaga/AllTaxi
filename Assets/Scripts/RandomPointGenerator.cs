508062dfab7263fdfaf23b2db29b694cdc910ce2
Isa <isabelcoboruizazuaga@gmail.com>
2024-05-14 11:10:34 +0200

    cleaned the repository a little

--- Assets/Scripts/RandomPointGenerator.cs
+++ Assets/Scripts/RandomPointGenerator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectDetectionController : MonoBehaviour
+public class RandomPointGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
