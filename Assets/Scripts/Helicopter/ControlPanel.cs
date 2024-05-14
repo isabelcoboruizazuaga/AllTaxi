25e962747659d7f0aa031b5855e46adaa591dbb1
Isa <isabelcoboruizazuaga@gmail.com>
2024-05-14 09:03:03 +0200

    added helicopter with controls

---
+++ Assets/Scripts/Helicopter/ControlPanel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanel : MonoBehaviour {
+    public AudioSource MusicSound;
+
+    [SerializeField]
+    KeyCode SpeedUp = KeyCode.Space;
+    [SerializeField]
+    KeyCode SpeedDown = KeyCode.LeftShift;
+    [SerializeField]
+    KeyCode Forward = KeyCode.W;
+    [SerializeField]
+    KeyCode Back = KeyCode.S;
+    [SerializeField]
+    KeyCode Left = KeyCode.A;
+    [SerializeField]
+    KeyCode Right = KeyCode.D;
+    [SerializeField]
+    KeyCode TurnLeft = KeyCode.Q;
+    [SerializeField]
+    KeyCode TurnRight = KeyCode.E;
+    
+    private KeyCode[] keyCodes;
+
+    public Action<PressedKeyCode[]> KeyPressed;
+    private void Awake()
+    {
+        keyCodes = new[] {
+                            SpeedUp,
+                            SpeedDown,
+                            Forward,
+                            Back,
+                            Left,
+                            Right,
+                            TurnLeft,
+                            TurnRight
+                        };
+
+    }
+
+    void Start () {
+	
+	}
+
+	void FixedUpdate ()
+	{
+	    var pressedKeyCode = new List<PressedKeyCode>();
+	    for (int index = 0; index < keyCodes.Length; index++)
+	    {
+	        var keyCode = keyCodes[index];
+	        if (Input.GetKey(keyCode))
+                pressedKeyCode.Add((PressedKeyCode)index);
+	    }
+
+	    if (KeyPressed != null)
+	        KeyPressed(pressedKeyCode.ToArray());       
+      
+	}
+}
